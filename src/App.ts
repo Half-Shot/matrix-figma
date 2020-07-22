375aefc647c588cf3c8c3c01d34f844775f454ef
Half-Shot <will@half-shot.uk>
2020-07-22 12:01:25 +0100

    Bot fiddlyness

--- src/App.ts
+++ src/App.ts
@@ -2,10 +2,12 @@ import * as Figma from "figma-js";
 import express from "express";
 import e, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { MatrixClient, MatrixEvent, MessageEventContent, MembershipEventContent, AutojoinUpgradedRoomsMixin, StateEvent } from "matrix-bot-sdk";
+import { MatrixClient, MatrixEvent, MessageEventContent, MembershipEventContent, AutojoinUpgradedRoomsMixin, StateEvent, LogService, LogLevel } from "matrix-bot-sdk";
 import config from "./config";
 import { FigmaRoomStateGlobalConfigEventType, IFigmaRoomStateGlobalConfig, FigmaFileRoom, IFigmaRoomStateFile, FigmaRoomStateFileEventType } from "./FigmaRoom";
 import { IFigmaPayload } from "./IPayload";
+
+LogService.setLevel(LogLevel.INFO);
 
 class FigmaApp {
     private figma: Figma.ClientInterface;
@@ -31,41 +33,52 @@ class FigmaApp {
     }
 
     private async onInvite(roomId: string, event: MatrixEvent<MembershipEventContent>) {
+        if (!this.globalState) {
+            // Still starting up, ignore
+            return;
+        }
         if (!this.globalState.adminUsers.includes(event.sender)) {
             console.warn(`Rejecting invite from ${event.sender} because they are not an admin`);
             await this.matrixClient.kickUser(this.matrixClient.getUserId(), roomId, "User is not on the permitted admin user list");
             return;
         }
         await this.matrixClient.joinRoom(roomId);
-        await this.matrixClient.sendNotice(roomId, "Hello 👋. Please say `figma add <fileId>` to start tracking comments for a file.");
+        let existingRoom = false;
+        if (!existingRoom) {
+            await this.matrixClient.sendNotice(roomId, "Hello 👋. Please give me moderator permissions, and say `figma add <fileId>` to start tracking comments for a file.");
+        }
     }
 
-    private async onRoomEvent(roomId: string, event: StateEvent<IFigmaRoomStateGlobalConfig|IFigmaRoomStateFile>) {
-        console.debug("onRoomEvent => ", roomId, event.eventId, event.type, event.stateKey);
-        if (event.sender !== this.myUserId || !this.globalState.adminUsers.includes(event.sender)) {
-            // Disallowed update, we don't trust these users.
-            console.warn(`Ignoring message from ${event.sender}, not an admin`);
+    private async onRoomEvent(roomId: string, event: any) {
+        if (event.unsigned?.age && event.unsigned?.age > 15000) {
+            console.log("ignoring old event");
         }
-        if (event.type === FigmaRoomStateFileEventType && event.stateKey) {
+        console.debug("onRoomEvent => ", roomId, event.event_id, event.type, event.state_key);
+        if (event.type === FigmaRoomStateFileEventType && event.state_key) {
             // Do we have a room for this already?
-            const existingRoom = this.figmaRooms.find((r) => r.roomId === roomId && r.stateKey === event.stateKey);
+            const existingRoom = this.figmaRooms.find((r) => r.roomId === roomId && r.stateKey === event.state_key);
             if (existingRoom) {
-                console.log("Updating state for existing room", event.content);
+                console.log("Updating state for existing room", event);
                 existingRoom.updateState(event.content as IFigmaRoomStateFile);
             } else {
                 // Create a new room.
-                console.log("Created new room from state", event.content);
-                this.figmaRooms.push(new FigmaFileRoom(roomId, event.stateKey, event.content as IFigmaRoomStateFile, this.matrixClient));
+                console.log("Created new room from state", event);
+                const state = event.content as IFigmaRoomStateFile;
+                await this.matrixClient.sendNotice(roomId, `Excellent! I am tracking ${state.fileId}.`);
+                this.figmaRooms.push(new FigmaFileRoom(roomId, event.state_key, state, this.matrixClient));
             }
-        } else if (event.type === FigmaRoomStateGlobalConfigEventType && event.stateKey === "" && roomId === config.adminRoom) {
+        } else if (event.type === FigmaRoomStateGlobalConfigEventType && event.state_key === "" && roomId === config.adminRoom) {
             console.log("Updating global config to", event.content);
             this.globalState = event.content as IFigmaRoomStateGlobalConfig;
         }
         // Otherwise, ignore the event.
     } 
 
-    private async onRoomMessage(roomId: string, event: MatrixEvent<MessageEventContent>) {
+    private async onRoomMessage(roomId: string, event: any) {
         console.debug("onRoomMessage => ", roomId, event.type, event.sender);
+        if (event.unsigned?.age && event.unsigned.age > 15000) {
+            console.log("ignoring old event");
+        }
         if (!event.content.body || event.sender === this.myUserId) {
             // Needs to be a message.
             return;
@@ -77,12 +90,19 @@ class FigmaApp {
             const result = /figma add ([A-Za-z]+)/.exec(event.content.body);
             if (result) {
                 // It is!
-                await FigmaFileRoom.createState(roomId, result[1], this.matrixClient);
+                let resultEmoji = "✅";
+                try {
+                    await FigmaFileRoom.createState(roomId, result[1], this.matrixClient);
+                } catch (ex) {
+                    await this.matrixClient.sendNotice(roomId, "Sorry, I need permission to send state events in order to start tracking. You can revoke the permission afterwards.");
+                    resultEmoji = "❌";
+                    return;
+                }
                 await this.matrixClient.sendEvent(roomId, "m.reaction", {
                     "m.relates_to": {
                         rel_type: "m.annotation",
-                        event_id: event.raw.event_id,
-                        key: "✅",
+                        event_id: event.event_id,
+                        key: resultEmoji,
                     }
                 });
                 // We don't need to push it, we will get the state reflected back.
@@ -111,7 +131,7 @@ class FigmaApp {
                 for (const event of roomState) {
                     if (event.type === FigmaRoomStateFileEventType) {
                         console.log("Created new room from state", roomId, event.content);
-                        this.figmaRooms.push(new FigmaFileRoom(roomId, event.stateKey, event.content as IFigmaRoomStateFile, this.matrixClient));
+                        this.figmaRooms.push(new FigmaFileRoom(roomId, event.state_key, event.content as IFigmaRoomStateFile, this.matrixClient));
                     }
                     // Else, ignore.
                 }
@@ -151,8 +171,6 @@ class FigmaApp {
     }
 
     public async startup() {    
-        console.log("Starting up...");
-        await this.matrixClient.start();
         console.log("Syncing rooms...");
         await this.syncRooms();
         this.myUserId = await this.matrixClient.getUserId();
@@ -161,7 +179,8 @@ class FigmaApp {
         while(this.globalState === undefined) {
             try {
                 await this.matrixClient.joinRoom(config.adminRoom);
-                this.globalState = await this.matrixClient.getRoomStateEvent(config.adminRoom, FigmaRoomStateGlobalConfigEventType, "");    
+                this.globalState = await this.matrixClient.getRoomStateEvent(config.adminRoom, FigmaRoomStateGlobalConfigEventType, ""); 
+                console.log(this.globalState);   
             } catch (ex) {
                 console.error(`Could not start, waiting for ${FigmaRoomStateGlobalConfigEventType} to be defined in ${config.adminRoom}. Waiting 5s`);
                 await new Promise(res => setTimeout(res, 5000));
@@ -173,6 +192,8 @@ class FigmaApp {
             .post("/", this.onWebhook.bind(this))
             .listen(9898);
         console.log(`Listening on http://0.0.0.0:9898`);
+        console.log("Starting matrix sync..");
+        await this.matrixClient.start();
     }
 }
 
